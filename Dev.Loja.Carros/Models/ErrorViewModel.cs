5a1c54995f0dc848b5ab238a5550a2f66a907a09
tiagoalfa <tiagoferreirafonsecaa@gmail.com>
2021-08-18 15:40:51 -0300

    Add project files.

---
+++ Dev.Loja.Carros/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dev.Loja.Carros.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
